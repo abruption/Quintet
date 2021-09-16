432dc2f04fbb8c0a657a0cb393b260f38ce6a0c8
abruption <yg.lee@quintet.co.kr>
2021-09-16 13:00:58 +0900

    Update README.md

--- DesignPattern/test.js
+++ DesignPattern/test.js
@@ -1,6 +1,6 @@
 // 익명의 함수를 사용하는 콜백함수 예제
 let temp = [1, 2, 3, 4, 5]
-temp.forEach(x => {console.log(x * 2)})
+temp.forEach(x => console.log(x * 2))
 
 // 함수의 이름과 값을 넘기는 콜백 함수
 function doSomething(number, callback) {
